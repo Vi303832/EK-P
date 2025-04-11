55b0a9a20affba5c52ec4aa4fb6a73d80d459d32
Akif <mehmetakiftanyerii@hotmail.com>
2025-04-11 17:40:26 +0300

    Slider improvement

--- src/components/ProjSlide.jsx
+++ src/components/ProjSlide.jsx
@@ -29,42 +29,52 @@ const ProjectSlide = ({ selected }) => {
     }, [selected]);
 
     return (
-        <Swiper
-            modules={[Navigation, Pagination]}
-            navigation
-            pagination={{ clickable: true }}
-            slidesPerView={4}
-            spaceBetween={20}
-            breakpoints={{
-                0: {
-                    slidesPerView: 1,
-                },
-                504: {
-                    slidesPerView: 2,
+        <div className="relative">
+            <Swiper
+                modules={[Navigation, Pagination]}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                }}
+                pagination={{
+                    clickable: true,
+                    dynamicBullets: true,
+                }}
+                slidesPerView={4}
+                spaceBetween={20}
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1,
+                    },
+                    504: {
+                        slidesPerView: 2,
+                    },
+                    640: {
+                        slidesPerView: 3,
+                        spaceBetween: 40
+                    },
+                    1100: {
+                        slidesPerView: 3,
+                        spaceBetween: 80
+                    },
+                    1400: {
+                        slidesPerView: 4,
+                        spaceBetween: 110
+                    }
+                }}
+                className="w-full h-full"
+            >
+                {slides && slides.map((slide, index) => (
+                    <SwiperSlide key={index}>
+                        <PCard slide={slide} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
 
-                },
-                640: {
-                    slidesPerView: 3,
-                    spaceBetween: 40
-                },
-                1100: {
-                    slidesPerView: 3,
-                    spaceBetween: 80
-                },
-                // when window width is >= 1280px (xl)
-                1400: {
-                    slidesPerView: 4,
-                    spaceBetween: 110
-                }
-            }}
-            className="w-full h-full"
-        >
-            {slides && slides.map((slide, index) => (
-                <SwiperSlide key={index}>
-                    <PCard slide={slide} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
+            {/* Custom Navigation Buttons */}
+            <div className="swiper-button-prev !-left-16 !text-white !w-10 !h-10 hover:!text-amber-400 transition-all duration-300 max-lg:!-left-4"></div>
+            <div className="swiper-button-next !-right-16 !text-white !w-10 !h-10 hover:!text-amber-400 transition-all duration-300 max-lg:!-right-4"></div>
+        </div>
     );
 };
 
